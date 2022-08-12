c204b1911a7c1ce8e5c88fc1957204228a81adf0
Cristian Dugacicu <razorcd@yahoo.com>
2022-08-12 21:41:38 +0200

    adds  predict docs

--- capstone-project/server/predict.py
+++ capstone-project/server/predict.py
@@ -1,3 +1,6 @@
+# run: gunicorn --bind=0.0.0.0:9696 --chdir=server --log-level=debug  predict:app
+# predict: curl -X POST -H 'Content-Type: application/json' localhost:9696/predict -d '{"customer_age":100,"gender":"F","dependent_count":2,"education_level":2,"marital_status":"married","income_category":2,"card_category":"blue","months_on_book":6,"total_relationship_count":3,"credit_limit":4000,"total_revolving_bal":2500}'
+
 from ast import Mod
 from multiprocessing.pool import RUN
 import os
