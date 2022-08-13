1e2c72ef22b278af71fb59d8cacf07777c618765
Cristian Dugacicu <razorcd@yahoo.com>
2022-08-13 21:20:20 +0200

    fix send_data script

--- capstone-project/server/send_data.py
+++ capstone-project/server/send_data.py
@@ -7,7 +7,7 @@ import requests
 
 SERVE_ADDRESS = os.getenv('SERVE_ADDRESS', 'http://127.0.0.1:9696/predict')
 
-table = pcsv.read_csv("input_clean/credit_card_churn_clean.csv")
+table = pcsv.read_csv("../input_clean/credit_card_churn_clean.csv")
 data = table.to_pylist()
 # {'': 0, 'customer_id': 768805383, 'customer_age': 45, 'gender': 'M', 'dependent_count': 3, 'education_level': 1, 'marital_status': 'married', 
 # 'income_category': 2, 'card_category': 'blue', 'months_on_book': 39, 'total_relationship_count': 5, 'credit_limit': 12691.0, 
@@ -29,10 +29,11 @@ for row in data:
       'total_revolving_bal': row['total_revolving_bal'],
     }
 
-    print(f'Request: {payload}')
-    resp = requests.post(SERVE_ADDRESS,
-                            headers={"Content-Type": "application/json"},
-                            data=json.dumps(row)) \
-                    .json()
-    print(f"Response: {resp}\n")
-    sleep(30*1)
+    while True:
+      print(f'Request: {payload}')
+      resp = requests.post(SERVE_ADDRESS,
+                              headers={"Content-Type": "application/json"},
+                              data=json.dumps(row)) \
+                      .json()
+      print(f"Response: {resp}\n")
+      sleep(30*1)
