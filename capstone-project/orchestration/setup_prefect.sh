1c66719bf500decc9d5bdb60e5a045c134a71975
Cristian Dugacicu <razorcd@yahoo.com>
2022-08-14 18:37:24 +0200

    makefile cleanup

--- capstone-project/orchestration/setup_prefect.sh
+++ capstone-project/orchestration/setup_prefect.sh
@@ -5,7 +5,7 @@ set -e
 echo "Setting up Prefect."
 
 echo "Create working queue."
-pipenv run prefect work-queue create work_queue_3
+prefect work-queue create work_queue_3
 
 echo "Creating storage. (interactive)"
 printf "1\n/tmp/store\n\nstoreName\ny\n" | prefect storage create
