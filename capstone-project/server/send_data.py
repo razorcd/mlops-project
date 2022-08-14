cbd54c6f744ff2634774f6fd100b75526ee5c427
Cristian Dugacicu <razorcd@yahoo.com>
2022-08-14 21:04:57 +0200

    send_data and env CLOUD_ADDRESS

--- capstone-project/server/send_data.py
+++ capstone-project/server/send_data.py
@@ -7,7 +7,7 @@ import requests
 
 SERVE_ADDRESS = os.getenv('SERVE_ADDRESS', 'http://127.0.0.1:9696/predict')
 
-table = pcsv.read_csv("../input_clean/credit_card_churn_clean.csv")
+table = pcsv.read_csv("input_clean/credit_card_churn_clean.csv")
 data = table.to_pylist()
 # {'': 0, 'customer_id': 768805383, 'customer_age': 45, 'gender': 'M', 'dependent_count': 3, 'education_level': 1, 'marital_status': 'married', 
 # 'income_category': 2, 'card_category': 'blue', 'months_on_book': 39, 'total_relationship_count': 5, 'credit_limit': 12691.0, 
