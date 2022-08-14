6467b223bbaffc1fbf0e1017015811f870497c6d
Cristian Dugacicu <razorcd@yahoo.com>
2022-08-14 19:33:36 +0200

    move from pipenv to pip in containers

--- capstone-project/orchestration/setup_prefect.sh
+++ capstone-project/orchestration/setup_prefect.sh
@@ -5,7 +5,7 @@ set -e
 echo "Setting up Prefect."
 
 echo "Create working queue."
-prefect work-queue create work_queue_3
+prefect work-queue create work_queue_3 || true
 
 echo "Creating storage. (interactive)"
 printf "1\n/tmp/store\n\nstoreName\ny\n" | prefect storage create
